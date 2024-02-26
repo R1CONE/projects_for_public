84ba033ec32b2ad08770988889b55dbc94e901f1
R1CONE <116386177+R1CONE@users.noreply.github.com>
2024-02-26 13:31:57 +0100

    Delete leet_code/1_two_sum.py

--- leet_code/1_two_sum.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[j] == target - nums[i]:
-                    return [i, j]
-        
