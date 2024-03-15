6b54176a62ae3ded507734bf2394b6b3d9eb2385
R1CONE <116386177+R1CONE@users.noreply.github.com>
2024-03-15 19:48:53 +0100

    Update plusplus.cpp

--- весь мусор/plusplus.cpp
+++ весь мусор/plusplus.cpp
@@ -369,4 +369,32 @@ int main()
     return 0;
 }
 
+///
+
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main() {
+	ofstream file("text.txt", ios_base::out);
+
+	if (file.is_open()) {
+		file << "Hello world";
+		file.close();
+	};
+	
+
+	ifstream file("text.txt");
+	if(file.is_open()){
+		//string temp;
+		//file >> temp;
+		char temp[100];
+		file.getline(temp, 100);
+		cout << temp << endl;
+		file.close();
+	
+	}
+};
+
 
