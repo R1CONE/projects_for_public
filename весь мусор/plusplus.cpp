c2fb64464cbb5c58753cefcb638815802592756f
R1CONE <116386177+R1CONE@users.noreply.github.com>
2024-03-29 11:43:15 +0100

    Update plusplus.cpp

--- весь мусор/plusplus.cpp
+++ весь мусор/plusplus.cpp
@@ -559,3 +559,39 @@ int main() {
 
     return 0;
 }
+
+/////
+
+class Person;
+class Dog {
+private:
+    int health = 100;
+public:
+    friend class Person;
+
+};
+
+class Person {
+public:
+    void Damage(Dog& dog) {
+        dog.health -= 20;
+        cout << "Health of animal: " << dog.health << endl;
+
+    }
+
+    void Heal(Dog& dog) {
+        dog.health += 10;
+        cout << "Health of animal: " << dog.health << endl;
+    }
+};
+int main() {
+
+    Dog skuby;
+    Person Alex;
+    Alex.Damage(skuby);
+    Alex.Heal(skuby);
+    Alex.Damage(skuby);
+    Alex.Damage(skuby);
+
+    return 0;
+}
