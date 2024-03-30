2aee20aa6721df654ffa233e47dea3b193054be4
R1CONE <116386177+R1CONE@users.noreply.github.com>
2024-03-30 20:52:31 +0100

    Create 13_Roman to Integer.cpp

---
+++ leet_code/c++/13_Roman to Integer.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> value;
+
+        value['I'] = 1;
+        value['V'] = 5;
+        value['X'] = 10;
+        value['L'] = 50;
+        value['C'] = 100;
+        value['D'] = 500;
+        value['M'] = 1000;
+
+        int ans = 0;
+        int length = s.length();
+
+        for (int i = 0; i < length - 1; i++){
+            if(value[s[i]] < value[s[i+1]])
+            {
+                ans -= value[s[i]];
+            }
+            else
+            {
+                ans += value[s[i]];
+            }
+        }
+        ans += value[s[length-1]];
+        return ans;
+    }
+};
