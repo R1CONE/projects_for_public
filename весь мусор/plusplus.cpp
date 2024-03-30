ffcbd35fc89a75e849cd5891e2ce5740c8faa324
R1CONE <116386177+R1CONE@users.noreply.github.com>
2024-03-30 16:41:07 +0100

    Update plusplus.cpp

--- весь мусор/plusplus.cpp
+++ весь мусор/plusplus.cpp
@@ -650,3 +650,44 @@ int main() {
 
     return 0;
 }
+
+///
+
+using namespace std;
+
+template <class T, class T2> //шаблон класс по сути просто замена int на T2
+class Arrays {
+private:
+    T2 len;
+    T* arr;
+
+public:
+    Arrays(T* a, T2 len) {
+        this->len = len;
+        arr = new T[len];
+        for (int i = 0; i < len; i++)
+            arr[i] = a[i];
+    }
+
+    void get() {
+        for (int i = 0; i < len; i++)
+            cout << arr[i] << " ";
+        cout << endl;
+    }
+
+    ~Arrays() {
+        delete[] arr;
+    }
+};
+
+int main() {
+    int arr1[] = { 3, 6, 3 };
+    Arrays<int, int> obj1(arr1, 3);
+    obj1.get();
+
+    float arr2[] = { 3.12f, 6.67f, 3.16f };
+    Arrays<float, int> obj2(arr2, 3);
+    obj2.get();
+
+    return 0;
+}
