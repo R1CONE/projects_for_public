277008dccf991e4da18d9f52f0aa20cff26fc1d2
R1CONE <116386177+R1CONE@users.noreply.github.com>
2024-05-06 11:06:30 +0200

    Update notes.sql

--- весь мусор/notes.sql
+++ весь мусор/notes.sql
@@ -3,3 +3,6 @@ SELECT vict_age,count(*) FROM crimes GROUP BY vict_age; // выписавает 
 SELECT vict_age,vict_sex,count(*) FROM crimes
 GROUP BY vict_age, vict_sex
 HAVING count(*) BETWEEN 5000 AND 6000
+
+
+SELECT occ_date, vict_age, count(*) FROM crimes GROUP BY vict_age; // группировка по возрасту и количеству
