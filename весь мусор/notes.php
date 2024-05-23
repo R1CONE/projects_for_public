7afe77bf51a55e47abd14e97149c012bfac174cb
R1CONE <116386177+R1CONE@users.noreply.github.com>
2024-05-23 10:43:00 +0200

    Update notes.php

--- весь мусор/notes.php
+++ весь мусор/notes.php
@@ -3,10 +3,22 @@
         $i = 1;
         $potenga = 1;
         while ( $i <= $wykladnik){
-            $potenga = $potenga * $podstawa;
+            $potenga = $potenga * $podstawa; //степень
             $i++;
         }
         return $potenga;
     }
     echo potenga(12, 2) . "<br>"; //144
 ?>
+
+<?php
+$servername = "localhost";
+$username = "root";
+$dbPassword = "";
+$database = "tests_prog";
+
+$conn = new mysqli($servername, $username, $dbPassword, $database);
+
+if ($conn->connect_error) {
+    die("Błąd połączenia: " . $conn->connect_error);
+}
