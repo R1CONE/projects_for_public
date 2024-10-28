6a9566bb7aaa70c9c96d75e0fee1f3b9cd2d89cd
R1CONE <116386177+R1CONE@users.noreply.github.com>
2024-10-28 08:35:58 +0100

    Update notes.sql

--- весь мусор/notes.sql
+++ весь мусор/notes.sql
@@ -6,3 +6,12 @@ HAVING count(*) BETWEEN 5000 AND 6000
 
 
 SELECT occ_date, vict_age, count(*) FROM crimes GROUP BY vict_age; // группировка по возрасту и количеству
+
+
+CREATE TABLE dane (
+    imie VARCHAR(50) NOT NULL,
+    nazwisko VARCHAR(50) NOT NULL,
+    wiek INT(3) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    PRIMARY KEY (nazwisko)
+);
