42ccc9a9d2fd0a82ea0ea3a8b3eb9c2f69d53a2d
R1CONE <116386177+R1CONE@users.noreply.github.com>
2025-05-07 15:16:09 +0200

    Update mysql_command.sql

--- весь мусор/mysql_command.sql
+++ весь мусор/mysql_command.sql
@@ -2,3 +2,7 @@ mysqldump -u root -p baza_osoby > C:\export.sql  ==> export
 
 mysql -u root -p baza_osoby < C:\export.sql  ==> import
 
+UPDATE 'people' SET 'name' = 'Maxim' WHERE id = 5; - обновить табличку table где id = 5 там будет name Mixim
+
+
+
