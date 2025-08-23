db32a6dac22da39a456139666fbd5a70f7c5c2c8
R1CONE <116386177+R1CONE@users.noreply.github.com>
2025-08-23 15:23:36 +0200

    Update baza.kt

--- android studio/baza.kt
+++ android studio/baza.kt
@@ -35,3 +35,39 @@ fun NewText() {
     }
     
 }
+
+
+
+
+package com.example.gdiejava
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Row(
+                modifier = Modifier
+                    .background(Color.Green)
+                    .fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(text = "Hello")
+                Text(text = "World")
+                Text(text = "!")
+            }
+        }
+    }
+}
