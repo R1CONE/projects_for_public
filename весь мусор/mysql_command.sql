61b411fe4d77db3fb04726b51a87e14fe41774a0
R1CONE <116386177+R1CONE@users.noreply.github.com>
2026-03-24 10:30:38 +0100

    Add stored procedure for searching all tables

--- весь мусор/mysql_command.sql
+++ весь мусор/mysql_command.sql
@@ -209,3 +209,77 @@ SELECT
 FROM CDN.OpisKadry ok
 JOIN CDN.Dzialy dz 
     ON ok.OPK_DzlId = dz.DZL_DzlId;
+
+
+USE [CDN_Firma_Demo_1_1]
+GO
+/****** Object:  StoredProcedure [dbo].[SearchAllTables]    Script Date: 24.03.2026 10:29:55 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- Source - https://stackoverflow.com/a/19846145
+-- Posted by Rafi, modified by community. See post 'Timeline' for change history
+-- Retrieved 2026-03-20, License - CC BY-SA 3.0
+
+ALTER PROC [dbo].[SearchAllTables]
+(
+@SearchStr nvarchar(100)
+)
+AS
+BEGIN
+
+    CREATE TABLE #Results (ColumnName nvarchar(370), ColumnValue nvarchar(3630))
+
+    SET NOCOUNT ON
+
+    DECLARE @TableName nvarchar(256), @ColumnName nvarchar(128), @SearchStr2 nvarchar(110)
+    SET  @TableName = ''
+    SET @SearchStr2 = QUOTENAME('%' + @SearchStr + '%','''')
+
+    WHILE @TableName IS NOT NULL
+
+    BEGIN
+        SET @ColumnName = ''
+        SET @TableName = 
+        (
+            SELECT MIN(QUOTENAME(TABLE_SCHEMA) + '.' + QUOTENAME(TABLE_NAME))
+            FROM     INFORMATION_SCHEMA.TABLES
+            WHERE         TABLE_TYPE = 'BASE TABLE'
+                AND    QUOTENAME(TABLE_SCHEMA) + '.' + QUOTENAME(TABLE_NAME) > @TableName
+                AND    OBJECTPROPERTY(
+                        OBJECT_ID(
+                            QUOTENAME(TABLE_SCHEMA) + '.' + QUOTENAME(TABLE_NAME)
+                             ), 'IsMSShipped'
+                               ) = 0
+        )
+
+        WHILE (@TableName IS NOT NULL) AND (@ColumnName IS NOT NULL)
+
+        BEGIN
+            SET @ColumnName =
+            (
+                SELECT MIN(QUOTENAME(COLUMN_NAME))
+                FROM     INFORMATION_SCHEMA.COLUMNS
+                WHERE         TABLE_SCHEMA    = PARSENAME(@TableName, 2)
+                    AND    TABLE_NAME    = PARSENAME(@TableName, 1)
+                    AND    DATA_TYPE IN ('char', 'varchar', 'nchar', 'nvarchar', 'int', 'decimal')
+                    AND    QUOTENAME(COLUMN_NAME) > @ColumnName
+            )
+
+            IF @ColumnName IS NOT NULL
+
+            BEGIN
+                INSERT INTO #Results
+                EXEC
+                (
+                    'SELECT ''' + @TableName + '.' + @ColumnName + ''', LEFT(' + @ColumnName + ', 3630) 
+                    FROM ' + @TableName + 'WITH (NOLOCK) ' +
+                    ' WHERE ' + @ColumnName + ' LIKE ' + @SearchStr2
+                )
+            END
+        END    
+    END
+
+    SELECT ColumnName, ColumnValue FROM #Results
+END 
